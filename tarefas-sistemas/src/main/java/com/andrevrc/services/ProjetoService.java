a9f9c7679eca308fede0d4297d5f99d19f871d4a
André Carvalho <andrvc782@gmail.com>
2025-01-27 00:37:59 -0300

    fix: Spring Security

--- tarefas-sistemas/src/main/java/com/andrevrc/services/ProjetoService.java
+++ tarefas-sistemas/src/main/java/com/andrevrc/services/ProjetoService.java
@@ -12,14 +12,9 @@ import com.andrevrc.repositories.ProjetoRepository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
-
 
 @RestController
 public class ProjetoService {
